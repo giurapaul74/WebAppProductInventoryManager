499995184d31b6ee9a01de551749f96339e51498
Giura Savu Paul Marian <giurapaul74@gmail.com>
2022-11-20 17:06:29 +0200

    Added project for managing models

---
+++ ProductInventoryManagerWeb/ProductInventoryManagerWeb.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using ProductInventoryManagerWeb.Interfaces;
+
+namespace ProductInventoryManagerWeb.Models
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
